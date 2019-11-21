0be0601aa5f16f26adcd000ad11fe3b81256a8c2
Mu Chen <muchen@Mus-MacBook-Pro-2.local>
2019-11-21 15:21:19 -0800

    add map page

---
+++ src/components/MapPage.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TopNavBar from "./TopNavBar"
+import App from "./App"
+import MapSideBar from "./MapSideBar"
+
+class MapPage extends React.Component {
+    render() {
+        return (
+            <div className="map-page">
+                <div className="nav-bar-other">
+                    <TopNavBar />
+                </div>
+                <div className="map-page-main">
+                    <MapSideBar />
+                    <App />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default MapPage;
