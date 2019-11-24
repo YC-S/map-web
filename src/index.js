01a763a95d37736b3b7f087a2b7eba9562418534
Mu Chen <muchen@Mus-MacBook-Pro-2.local>
2019-11-23 20:30:49 -0800

    Fixed Registration form bug

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from './serviceWorker';
 
 
 
-ReactDOM.render(<MapPage />, document.getElementById('root'));
+ReactDOM.render(<Landing />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
